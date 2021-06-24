09ec49d749ef0a610ec25d1da9a9058afeceac2e
leducquangpm <leducquangpm@gmail.com>
2021-06-24 23:44:54 +0700

    fix bugs, improve UI: more interaction in single sample view

--- web-src/src/components/Cards/Card.vue
+++ web-src/src/components/Cards/Card.vue
@@ -8,11 +8,12 @@
         <h4 class="card-title">{{title}}</h4>
         <p class="card-category" v-if="subTitle">{{subTitle}}</p>
       </slot>
+      <!-- <div style="float:right;cursor:pointer" v-on:click="isHidden = !isHidden"><i v-if="!isHidden" class="ti-angle-up"></i><i v-if="isHidden" class="ti-angle-down"></i></div> -->
     </div>
-    <div class="card-body" v-if="$slots.default">
+    <div  class="card-body" v-if="$slots.default&&!isHidden">
       <slot></slot>
     </div>
-    <slot name="raw-content"></slot>
+    <slot name="raw-content" v-if="!isHidden"></slot>
     <div class="card-footer" v-if="$slots.footer">
       <hr>
       <slot name="footer"></slot>
@@ -25,6 +26,12 @@ export default {
   props: {
     title: String,
     subTitle: String
+  },
+  data(){
+    return{
+       isHidden:false
+    }
+   
   }
 };
 </script>
