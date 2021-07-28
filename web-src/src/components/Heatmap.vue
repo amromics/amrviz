f4108ce633e8f61663d18f55b791ac9e2ef4e078
leducquangpm <leducquangpm@gmail.com>
2021-07-28 17:42:00 +0700

    allow download heatmap data

--- web-src/src/components/Heatmap.vue
+++ web-src/src/components/Heatmap.vue
@@ -5,6 +5,12 @@ select{
   padding: 4px;
   background-color: transparent;
 }
+.download-link{
+  text-decoration: underline;
+  color:blue;
+  float:right;
+  cursor: pointer;
+}
 </style>
 <template>
 <div>
@@ -12,6 +18,7 @@ select{
   <select v-if="list_type"  name="type" id="type" v-model="selected_data" v-on:change="onChangeData">
     <option v-for="item  in list_type" :key="item.name" :value="item.type">{{item.name}}</option>
   </select>
+  <a v-on:click="downloadCSV" id="download_link" class="download-link">Download CSV</a>
 </div>
 <div id="heatmap" style="width:100%">
 </div>
@@ -35,6 +42,7 @@ export default {
       list_vir_hits:[],
       list_amr_class:[],
       list_vir_class:[],
+      
       heatmapview:undefined,
       selected_data:"amr",
       tree_data:undefined,
@@ -53,6 +61,7 @@ export default {
         if(!this.list_amr_class.includes(this.heatmap.hits[i].class)){
           this.list_amr_class.push(this.heatmap.hits[i].class);
         }
+       
       }
       if(this.heatmap.hits[i].type=="vir"){
         this.list_vir_hits.push(this.heatmap.hits[i]);
@@ -77,6 +86,8 @@ export default {
       this.heatmapview.setActiveNames(arr_ids);
       
     });
+    //export csv  file
+    
     
     this.loading = false;
   },
@@ -99,7 +110,63 @@ export default {
               if (this.selected_data=="vir")
                   this.heatmapview.load(this.tree_data,this.list_vir_hits);
               this.heatmapview.draw();
+          },
+    downloadCSV(){
+       var data_to_export;
+        if (this.selected_data=="amr"){
+           data_to_export=this.list_amr_hits;
+           
+        }
+               
+        if (this.selected_data=="vir")
+               data_to_export=this.list_vir_hits;
+        var rows=[];
+       
+        var map_sample=new Map();
+        var map_gene=new Map();
+        var map=new Map();
+        console.log(data_to_export);
+        for (var i=0;i<data_to_export.length;i++){
+            if(!map_sample.has(data_to_export[i].sample)){
+              map_sample.set(data_to_export[i].sample,map_sample.size);
+            }
+            if(!map_gene.has(data_to_export[i].gene)){
+              map_gene.set(data_to_export[i].gene,map_gene.size);
+            }
+            map.set(map_sample.get(data_to_export[i].sample)+"-"+map_gene.get(data_to_export[i].gene),data_to_export[i].identity);
+
+        }
+        console.log(map);
+        var str='"sample",';
+        for(const[key,value] of map_gene){
+         
+          str+='"'+key+'",';
+        }
+        str=str.substring(0, str.length - 1)+'\n';
+        
+        for(const[key,value] of map_sample){
+         
+          var r_str='"'+key+'",';
+          for(const[key_g,value_g] of map_gene){
+            //console.log(value+"-"+value_g);
+            if(map.has(value+"-"+value_g))
+            
+              r_str+='"'+map.get(value+"-"+value_g)+'",';
+            else
+              r_str+='"",';      
           }
+          r_str=r_str.substring(0, r_str.length - 1)+'\n';
+           str+=r_str;
+        }
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(str);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = this.selected_data+'.csv';
+        hiddenElement.click();
+        console.log(rows);
+       
+        
+    }
   }
 };
 </script>
