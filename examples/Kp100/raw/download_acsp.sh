429ef33edc4ae98aa0e00f661460e6498d26ed43
hsnguyen <hoangnguyen.minor@gmail.com>
2024-04-01 10:54:39 +1000

    adding iGAS70 data downloaded with ascp

--- examples/Kp100/raw/download_acsp.sh
+++ examples/Kp100/raw/download_acsp.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-source activate amromics
+#Install IBM aspera-connect 4.1.3 (note that latest version doesn't work):
+if [ ! -f ~/.aspera/connect/bin/ascp ] || [ -f ~/.aspera/connect/etc/asperaweb_id_dsa.openssh ]
+then
+  wget https://ak-delivery04-mul.dhe.ibm.com/sar/CMA/OSA/0adrj/0/ibm-aspera-connect_4.1.3.93_linux.tar.gz
+  tar zxvf ibm-aspera-connect_4.1.3.93_linux.tar.gz
+  bash ibm-aspera-connect_4.1.3.93_linux.sh && rm ibm-aspera-connect_4.1.3.93_linux.*
+fi
+export PATH=~/.aspera/connect/bin/:$PATH
+KEYPATH=~/.aspera/connect/etc/asperaweb_id_dsa.openssh
+
+#Download assembly
 if [ ! -f GCF_000240185.1_ASM24018v2_genomic.fna.gz ]; then
     wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/240/185/GCF_000240185.1_ASM24018v2/GCF_000240185.1_ASM24018v2_genomic.fna.gz
 fi
@@ -7,7 +17,7 @@ if [ ! -f GCF_000364385.2_ASM36438v2_genomic.fna.gz ]; then
     wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/364/385/GCF_000364385.2_ASM36438v2/GCF_000364385.2_ASM36438v2_genomic.fna.gz
 fi
 
-### Faster downloading:
+#Download FASTQ by ascp
 # Get FTP link from EBI
 rm -f sra*txt
 awk -F"," '/Illumina/{print $1}' acc_list.csv| parallel -j 16 'curl -X GET "https://www.ebi.ac.uk/ena/portal/api/filereport?accession={}&fields=fastq_ftp&result=read_run" 2>/dev/null | sed -n "2 p" >> sra_short.ftp.txt'
@@ -17,7 +27,6 @@ awk -F"," '/Pacbio|ONT/{print $1}' acc_list.csv| parallel -j 16 'curl -X GET "ht
 sed 's/;/\t/g' sra_short.ftp.txt | sed 's/ftp.sra.ebi.ac.uk\//era-fasp@fasp.sra.ebi.ac.uk:/g' > sra_short.ascp.txt
 totN=$(cat sra_short.ascp.txt|wc -l)
 curN=0
-KEYPATH="${CONDA_PREFIX}"/opt/aspera/connect/etc/asperaweb_id_dsa.openssh
 while read s r1 r2
 do
 	curN=$((curN + 1))
